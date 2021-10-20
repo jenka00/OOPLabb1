1afac92ba10d9a532bf5dc767aadb2200867319f
Jenny Kallberg <jenka00@hotmail.com>
2021-10-20 22:37:36 +0200

    Add project files.

---
+++ OOPLabb1/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLabb1
+{
+    class Circle
+    {
+
+        float pi = 3.141f;
+        int Radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public void getArea()
+        {
+            Console.WriteLine(Radius * Radius * pi);
+        }
+    }
+}
---
+++ OOPLabb1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOPLabb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle firstCircle = new Circle(5);
+
+            Circle secondCircle = new Circle(6);
+
+            firstCircle.getArea();
+
+            secondCircle.getArea();
+        }
+    }
+}
